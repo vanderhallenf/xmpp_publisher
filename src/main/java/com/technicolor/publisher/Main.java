8ce2fae5f788bd33642ea8d26e83a5087e97322a
Juan Luis Pardo Gonzalez <juanluis.pardogonzalez@technicolor.com>
2012-09-24 14:02:38 +0200

    Revert "24/09/2012 - 13.26"
    This reverts commit 174f08b4030aeed243f593f5a6be957f75bca088.

--- src/main/java/com/technicolor/publisher/Main.java
+++ src/main/java/com/technicolor/publisher/Main.java
@@ -1,32 +1,20 @@
 package com.technicolor.publisher;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 
 public class Main {
 
-    public static void main(String[] args) throws XMPPException, IOException {
-        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
-        String str;
-        
-        //XMPPConnection.DEBUG_ENABLED = true;
+
+    public static void main(String[] args) throws XMPPException {
+        XMPPConnection.DEBUG_ENABLED = true;
         Publisher publisher = new Publisher();
         publisher.login("admin", "oyente");
-        System.out.println("Dame el item");
-        str = buff.readLine();
-        
-        
-        //publisher.addNode("Nodo1");
-        //publisher.deleteNode("Peliculas");
-        publisher.publish(str);
-        
-//        for (int i=1;i<6;i++){
-//            publisher.deleteNode(("Nodo"+i));
-//        }
-
+        //publisher.addNode("Mua");
+        publisher.publish();
+       
+ 
+        //publisher.configure();
 
     }
 }
--- src/main/java/com/technicolor/publisher/Publisher.java
+++ src/main/java/com/technicolor/publisher/Publisher.java
@@ -1,5 +1,7 @@
 package com.technicolor.publisher;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
@@ -8,38 +10,50 @@ import org.jivesoftware.smackx.pubsub.ConfigureForm;
 import org.jivesoftware.smackx.pubsub.FormType;
 import org.jivesoftware.smackx.pubsub.Item;
 import org.jivesoftware.smackx.pubsub.LeafNode;
+import org.jivesoftware.smackx.pubsub.PayloadItem;
 import org.jivesoftware.smackx.pubsub.PubSubManager;
 import org.jivesoftware.smackx.pubsub.PublishModel;
+import org.jivesoftware.smackx.pubsub.SimplePayload;
 
 public class Publisher {
 
     XMPPConnection connection;
     PubSubManager pubSubManager;
-    
 
     public Publisher() {
         ConnectionConfiguration config = new ConnectionConfiguration("cplx129.edegem.eu.thmulti.com");
         connection = new XMPPConnection(config);
         pubSubManager = new PubSubManager(connection);
-        
+
+
     }
 
     protected void login(String userName, String password) throws XMPPException {
-
+        
         connection.connect();
-
+        
         System.out.println("Is it connected? " + connection.isConnected());
         System.out.println("Is it authenticated? " + connection.isAuthenticated());
         connection.login(userName, password);
         System.out.println("Is it authenticated? " + connection.isAuthenticated());
     }
 
-    public void publish(String nombre) throws XMPPException {
+    public void publish() throws XMPPException {
+
+//        SimplePayload payload = new SimplePayload("Gladiator", "Peliculas", "Es una pelicula sobre un gladiador");
+//
         
-      LeafNode node = (LeafNode)pubSubManager.getNode("Nodo1");
-      node.send(new Item());
-      node.send(new Item(nombre));
-              
+         SimplePayload payload = new SimplePayload("book","pubsub:test:book", "<book xmlns='pubsub:test:book'><title>Lord of the Rings</title></book>");
+        PayloadItem<SimplePayload> payloadItem = new PayloadItem<SimplePayload>(null, payload);
+         ((LeafNode) pubSubManager.getNode("Musica")).publish(payloadItem);
+        
+        
+//        Item item = new Item("Kill Bill");
+//        ((LeafNode)pubSubManager.getNode("Peliculas")).publish(item);
+        
+             
+       System.out.println(((LeafNode) pubSubManager.getNode("Peliculas")).getItems().size());
+
     }
 
     public void addNode(String nodeName) throws XMPPException {
@@ -52,13 +66,8 @@ public class Publisher {
             form.setAccessModel(AccessModel.open);
             form.setPublishModel(PublishModel.open);
             form.setSubscribe(true);
-         
-            LeafNode n = (LeafNode) pubSubManager.createNode(nodeName, form);
-            
-            
-            
-        }else{
-            System.out.println("El nodo ya existe");
+            pubSubManager.createNode(nodeName);
+            LeafNode n = (LeafNode) pubSubManager.createNode("Trabalenguas", form);
         }
     }
 
@@ -67,19 +76,12 @@ public class Publisher {
             pubSubManager.getNode(nodeName);
             return true;
         } catch (XMPPException ex) {
-            System.out.println("Node " + nodeName + " didn't exist, it's been created");
+            System.out.println("El nodo" + nodeName + " no existe");
             return false;
         }
-    }
-
-    public void deleteNode(String id) throws XMPPException {
-        pubSubManager.deleteNode(id);
     }
 
     public void logout() {
         connection.disconnect();
     }
-    
-       
-    
 }
