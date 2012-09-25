c54f1d30e68a6ba95ca36284efdfd33e83a27985
Juan Luis Pardo Gonzalez <juanluis.pardogonzalez@technicolor.com>
2012-09-25 09:55:44 +0200

    25/09/2012 - 09.55 Home version

--- src/main/java/com/technicolor/publisher/Main.java
+++ src/main/java/com/technicolor/publisher/Main.java
@@ -1,17 +1,17 @@
 package com.technicolor.publisher;
 
-import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+
 
 public class Main {
 
 
     public static void main(String[] args) throws XMPPException {
-        XMPPConnection.DEBUG_ENABLED = true;
+        
         Publisher publisher = new Publisher();
         publisher.login("admin", "oyente");
-        publisher.addNode("Node2");
-        //publisher.publish();
-      
+        
+        //publisher.addNode();
+        publisher.publish();
     }
 }
--- src/main/java/com/technicolor/publisher/Publisher.java
+++ src/main/java/com/technicolor/publisher/Publisher.java
@@ -1,92 +1,52 @@
 package com.technicolor.publisher;
 
-import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.jivesoftware.smack.Connection;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smackx.pubsub.AccessModel;
-import org.jivesoftware.smackx.pubsub.ConfigureForm;
-import org.jivesoftware.smackx.pubsub.FormType;
-import org.jivesoftware.smackx.pubsub.Item;
-import org.jivesoftware.smackx.pubsub.LeafNode;
-import org.jivesoftware.smackx.pubsub.PayloadItem;
-import org.jivesoftware.smackx.pubsub.PubSubManager;
-import org.jivesoftware.smackx.pubsub.PublishModel;
-import org.jivesoftware.smackx.pubsub.SimplePayload;
+import org.jivesoftware.smackx.pubsub.*;
 
 public class Publisher {
 
-    XMPPConnection connection;
-    PubSubManager pubSubManager;
+    PubSubManager mgr;
+    Connection con;
 
-    public Publisher() {
-        ConnectionConfiguration config = new ConnectionConfiguration("cplx129.edegem.eu.thmulti.com");
-        connection = new XMPPConnection(config);
-        pubSubManager = new PubSubManager(connection);
+    public void Publisher() {
+        con = null;
+        mgr = null;
     }
 
-    protected void login(String userName, String password) throws XMPPException {
+    public void login(String username, String password) throws XMPPException {
+        ConnectionConfiguration config = new ConnectionConfiguration("cplx129.edegem.eu.thmulti.com");
+        con = new XMPPConnection(config);
+        con.connect();
+        con.login(username, password);
+    }
 
-        connection.connect();
+    public void addNode() throws XMPPException {
+        mgr = new PubSubManager(con);
+        LeafNode leaf = mgr.createNode("testNode");
+        ConfigureForm form = new ConfigureForm(FormType.submit);
+        form.setAccessModel(AccessModel.open);
+        form.setDeliverPayloads(false);
+        form.setNotifyRetract(true);
+        form.setPersistentItems(true);
+        form.setPublishModel(PublishModel.open);
+        form.setSubscribe(true);
+        form.setPresenceBasedDelivery(false);
 
-        System.out.println("Is it connected? " + connection.isConnected());
-        System.out.println("Is it authenticated? " + connection.isAuthenticated());
-        connection.login(userName, password);
-        System.out.println("Is it authenticated? " + connection.isAuthenticated());
+        leaf.sendConfigurationForm(form);
     }
 
     public void publish() throws XMPPException {
-
-
-        // Get the node
-        LeafNode node=null;
-     
-            //node = (LeafNode) pubSubManager.getNode("Node1");
-
-
-            // Publish an Item, let service set the id
-            //node.send(new Item("abc"));
-
-            SimplePayload payload = new SimplePayload("book", null, "<book xmlns='pubsub:book'><title>1984</title></book>");
-            PayloadItem<SimplePayload> payloadItem = new PayloadItem<SimplePayload>(null, payload);
-            ((LeafNode) pubSubManager.getNode("Node2")).publish(payloadItem);
-
-    //        Iterator it = (((LeafNode) pubSubManager.getNode("testNode4")).getItems()).iterator();
-    //        while (it.hasNext()) {
-    //            System.out.println(it.next());
-    //        }
-
-    }
-
-    public void addNode(String nodeName) throws XMPPException {
-        if (!existNode(nodeName)) {
-            ConfigureForm form = new ConfigureForm(FormType.submit);
-//            form.setPersistentItems(false);
-//            form.setDeliverPayloads(false);
-//            form.setNotifyDelete(true);
-//            form.setNotifyRetract(true);
-//            form.setAccessModel(AccessModel.open);
-//            form.setPublishModel(PublishModel.open);
-            form.setSubscribe(true);
-            
-            
-      form.setAccessModel(AccessModel.open);
-      form.setDeliverPayloads(true);
-      form.setNotifyRetract(true);
-      form.setPersistentItems(true);
-      form.setPublishModel(PublishModel.open);
-            
-            pubSubManager.createNode(nodeName);
-           // LeafNode n = (LeafNode) pubSubManager.createNode("Node1", form);
-              System.out.println("added node");
-        }
+        mgr = new PubSubManager(con);
+        LeafNode node = (LeafNode) mgr.getNode("testNode");
+        node.send(new PayloadItem("test6" + System.currentTimeMillis(), new SimplePayload("book", "pubsub:test6:book", "")));
     }
 
     public boolean existNode(String nodeName) {
         try {
-            pubSubManager.getNode(nodeName);
+            mgr.getNode(nodeName);
             return true;
         } catch (XMPPException ex) {
             System.out.println("El nodo" + nodeName + " no existe");
@@ -95,6 +55,6 @@ public class Publisher {
     }
 
     public void logout() {
-        connection.disconnect();
+        con.disconnect();
     }
 }
